c60d8f26368776b06ae3425f2acc59a574cea8d5
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-11-21 23:40:19 +0100

    V1.2.2
    Termine details PHP

---
+++ README.md
@@ -0,0 +1,14 @@
+JAVA
+
+J'ai simplement utilisé IntelliJ, qui est un excellent éditeur de code pour Java.
+
+Pour simplifier la tâche, il vous suffit d'avoir IntelliJ installé sur votre machine locale. Ensuite, vous pouvez télécharger mon projet depuis mon GitHub et l'ouvrir simplement dans IntelliJ. Une fois le projet ouvert, vous pouvez accéder à la classe de test et exécuter le fichier Java "ChangeReturnTest" qui se trouve dans le dossier "src".
+
+Si vous utilisez un éditeur de code différent, tel que VSCode, assurez-vous également d'avoir le dossier de mon projet, puis ouvrez-le dans VSCode.
+
+Sinon, vous pouvez copier-coller le code de mon projet dans un éditeur Java en ligne gratuit, tel que https://www.onlinegdb.com/online_java_compiler#, et simplement exécuter le programme.
+
+PHP
+J'ai utilisé XAMPP sur ma machine pour démarrer un serveur local. J'ai ensuite ajouté le dossier "TechnicalTest_PHP" au répertoire "htdoc". Enfin, j'ai pu accéder au dossier en allant à l'adresse http://localhost/TechnicalTest_PHP/. J'ai ensuite pu tester n'importe quel nombres.
+
+    
--- TechnicalTest_PHP/backend.php
+++ TechnicalTest_PHP/backend.php
@@ -1,24 +1,30 @@
 <?php
 
+// Vérifie si la requête est de type POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupére le montant envoyé via la requête POST
     $testAmount = $_POST["amount"];
 
     function giveChange($amount) {
-        $solution = array();
+        
+        $solution = array(); // Initialise un tableau pour stocker la solution
         $initialAmount = $amount;
         $numberOfBills = 0;
 
+        // Boucle tant que le montant est supérieur à 13 ou égal à 12 ou 10, plus d'explication dans le code java
         while ($amount > 13 || $amount == 12 || $amount == 10) {
             $numberOfBills++;
             $solution[10] = $numberOfBills;
             $amount -= 10;
         }
 
+        // Vérifie si le montant restant est impair
         if ($amount % 2 != 0) {
             $solution[5] = 1;
             $amount -= 5;
         }
 
+        // Gérer les cas exception pour 6 et 8
         if ($amount == 6) {
             $solution[2] = 3;
             $amount = 0;
@@ -26,7 +32,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $solution[4] = 2;
             $amount = 0;
         } else {
-            $numberOfBills =0;
+            $numberOfBills = 0;
+            // Boucle tant que le montant restant est supérieur à 0 pour la monnaie 2
             while ($amount > 0) {
                 $numberOfBills++;
                 $solution[2] = $numberOfBills;
@@ -34,26 +41,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
 
+        // Construit la chaîne de résultat
         $result = "<div class='result'>";
-        $result .= "<h2>Amount to return: $" . $initialAmount . "</h2>";
+        $result .= "<h2>Montant à rendre : $" . $initialAmount . "</h2>";
 
+        // Vérifie si le montant restant est devenu 0
         if ($amount == 0) {
-            $result .= "<div class='solution'>Optimal solution:</div>";
+            $result .= "<div class='solution'>Solution optimale :</div>";
             $result .= "<ul>";
             foreach ($solution as $key => $value) {
                 $result .= "<li>" . $value . " * $" . $key . "</li>";
             }
             $result .= "</ul>";
         } else {
-            $result .= "<div class='error'>Unable to give change.</div>";
+            // Affiche un message d'erreur si le montant ne peut pas être rendu pour 1 & 3
+            $result .= "<div class='error'>Impossible de rendre la monnaie.</div>";
         }
 
+        // Termine la construction de la chaîne de résultat
         $result .= "</div>";
         return $result;
     }
 
-    // Appel de la fonction et affichage du résultat
+    // Appel de la fonction et récupération du résultat
     $output = giveChange($testAmount);
+    // Afficher le résultat
     echo $output;
 }
 ?>
