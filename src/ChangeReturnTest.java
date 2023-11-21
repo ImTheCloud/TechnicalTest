a68e737af44888e6ab8b6f9c6d288c64ba34df69
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-11-21 20:22:26 +0100

    Update ChangeReturnTest.java

--- src/ChangeReturnTest.java
+++ src/ChangeReturnTest.java
@@ -17,12 +17,14 @@ public class ChangeReturnTest {
 
     // méthode utilitaire pour simplifier l'appel de la méthode de test
     private void testChangeForAmount(long amount, Map<Long, Long> expectedSolution) {
+        // appelle la méthode giveChange et vérifie si la solution correspond à celle attendue
         Map<Long, Long> actualSolution = ChangeReturn.giveChange(amount);
         assertEquals(expectedSolution, actualSolution);
     }
 
     // méthode utilitaire pour créer une Map à partir d'une liste variable de valeurs clé-valeur
     private Map<Long, Long> map(Long... values) {
+        // les valeurs proviennet de la ligne de testGiveChange
         Map<Long, Long> map = new HashMap<>();
         for (int i = 0; i < values.length; i += 2) {
             map.put(values[i], values[i + 1]);
